fcba80cd9353b3b9c95bce1e7ef0297165b45091
Pedro Silva <pedrosilvacwb@gmail.com>
2022-12-27 14:16:15 -0300

    update server

--- client/script.js
+++ client/script.js
@@ -67,7 +67,7 @@ const handleSubmit = async (e) => {
 
   loading(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://oraculo.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
